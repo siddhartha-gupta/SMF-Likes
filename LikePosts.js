05e2a047762bac40aee403c53a17c5b88826e424
Joker-SMF <makerzzz.zzzz@gmail.com>
2013-02-16 15:15:34 +0530

    Giving shape to basic mod structure

--- LikePosts.js
+++ LikePosts.js
@@ -1,1 +1,31 @@
-like_posts = {};
+var likePosts = function() {
+}
+
+likePosts.prototype.likeUnlikePosts = function(msgId, topicId, boardId) {
+    // Lets try JS validations
+    msgId = (msgId != undefined) ? parseInt(msgId) : 0;
+    topicId = (topicId != undefined) ? parseInt(topicId) : 0;
+    boardId = (boardId != undefined) ? parseInt(boardId) : 0;
+    
+    if(isNaN(msgId) || isNaN(topicId) || isNaN(boardId)) {
+        return false;
+    }
+
+    //console.log(url);
+    $.ajax({
+        type: "POST",
+        url: smf_scripturl + '?action=likeposts;sa=like_post;xml',
+        context: document.body,
+        data: {
+            msg: msgId,
+            topic: topicId,
+            board: boardId
+        },
+        
+        success: function(request){
+            console.log(request);
+        },
+    });
+}
+
+var lpObj = new likePosts();
