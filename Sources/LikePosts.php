9c753a26bce6b69306cb7f05005e0a74db668fb7
Sid <makerzzz.zzzz@gmail.com>
2013-12-07 19:07:06 +0530

    Versioning

--- Sources/LikePosts.php
+++ Sources/LikePosts.php
@@ -2,7 +2,7 @@
 
 /**
 * @package manifest file for Like Posts
-* @version 1.2
+* @version 1.2.1
 * @author Joker (http://www.simplemachines.org/community/index.php?action=profile;u=226111)
 * @copyright Copyright (c) 2012, Siddhartha Gupta
 * @license http://www.mozilla.org/MPL/MPL-1.1.html
