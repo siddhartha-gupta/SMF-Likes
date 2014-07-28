58bfc478707a1cdf67c33b19fd4544cc2a3d092b
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2014-07-28 19:29:47 +0530

    some left code

--- goodPostBadPostConverter.php
+++ goodPostBadPostConverter.php
@@ -58,6 +58,130 @@ function initGPBPConverter() {
 function convertToLike() {
 	global $smcFunc;
 
+	checkTableExists();
+	checkVersionofLP();
+}
+
+function checkTableExists() {
+	global $smcFunc, $db_prefix;
+
+	db_extend();
+	$tables = $smcFunc['db_list_tables']();
+	$real_prefix = preg_match('~^(`?)(.+?)\\1\\.(.*?)$~', $db_prefix, $match) === 1 ? $match[3] : $db_prefix;
+
+	if(!in_array($real_prefix . 'log_gpbp', $tables)) {
+		echo 'Oops! It looks like good post bad post table doesn\'t exist';
+		die();
+	} elseif(!in_array($real_prefix . 'like_post', $tables)) {
+		echo 'Oops! It looks like \'Like posts mod\' isn\'t installed';
+		die();
+	}
+}
+
+function checkVersionofLP() {
+	$latestVersion = false;
+	$request = $smcFunc['db_query']('', '
+		SHOW COLUMNS
+		FROM {db_prefix}like_post',
+		array(
+		)
+	);
+
+	if ($request !== false) {
+		while ($row = $smcFunc['db_fetch_assoc']($request)) {
+			if ($row['Field'] == 'liked_timestamp' && ($row['Type'] == 'int(10) unsigned' || $row['Type'] == 'int(10)'))
+				$latestVersion = true;
+		}
+		$smcFunc['db_free_result']($request);
+	}
+
+	if($latestVersion === false) {
+		echo 'Please upgrade like posts mod to latest version';
+		die();
+	}
+}
+
+function test() {
+	@set_time_limit(300);
+
+	$startLimit = !isset($_REQUEST['startLimit']) || empty($_REQUEST['startLimit']) ? 0 : (int) $_REQUEST['startLimit'];
+	$endLimit = (int) $_REQUEST['endLimit'];
+
+	if(!isset($_REQUEST['totalWork']) || empty($_REQUEST['totalWork'])) {
+		$request = $smcFunc['db_query']('', '
+			SELECT COUNT(id_member)
+			FROM {db_prefix}members'
+		);
+		list($totalWork) = $smcFunc['db_fetch_row']($request);
+		$smcFunc['db_free_result']($request);
+	} else {
+		$totalWork = (int) $_REQUEST['totalWork'];
+	}
+
+	$request = $smcFunc['db_query']('', '
+		SELECT id_member
+		FROM {db_prefix}members
+		LIMIT {int:start}, {int:max}',
+		array(
+			'start' => $startLimit,
+			'max' => 100,
+		)
+	);
+
+	$insertData = array();
+	$updateIds = array();
+	$updateData = '';
+	while ($row = $smcFunc['db_fetch_assoc']($request)) {
+		$calculatedLikeCount = 0;
+		$request1 = $smcFunc['db_query']('', '
+			SELECT COUNT(lp.id_member_received) as count, lc.like_count
+			FROM {db_prefix}like_post AS lp
+			INNER JOIN {db_prefix}messages AS m ON (m.id_msg = lp.id_msg)
+			LEFT JOIN {db_prefix}like_count AS lc ON (lc.id_member = {int:id_member})
+			where m.id_member = {int:id_member}',
+			array(
+				'id_member' => $row['id_member'],
+			)
+		);
+		if ($smcFunc['db_num_rows']($request1) !== 0) {
+			list ($calculatedLikeCount, $presentCount) = $smcFunc['db_fetch_row']($request1);
+			if($presentCount === NULL) {
+				$insertData[] = array($row['id_member'], $calculatedLikeCount);
+			} else if($calculatedLikeCount !== $presentCount) {
+				$updateIds[] = $row['id_member'];
+				$updateData .= '
+						WHEN ' . $row['id_member'] . ' THEN ' . $calculatedLikeCount;
+			}
+		} else {
+			$insertData[] = array($row['id_member'], $calculatedLikeCount);
+		}
+		$smcFunc['db_free_result']($request1);
+	}
+	$smcFunc['db_free_result']($request);
+
+	if(!empty($updateData) && !empty($updateIds)) {
+		$result = $smcFunc['db_query']('', '
+			UPDATE {db_prefix}like_count
+			SET like_count = CASE id_member '. $updateData .' END
+			WHERE id_member IN ({array_int:updateIds})',
+			array(
+				'updateIds' => $updateIds
+			)
+		);
+	}
+
+	if(!empty($insertData)) {
+		$result = $smcFunc['db_insert']('replace',
+			'{db_prefix}like_count',
+			array('id_member' => 'int', 'like_count' => 'int'),
+			$insertData,
+			array('id_member')
+		);
+	}
+
+	$resp = array('totalWork' => (int) $totalWork, 'endLimit' => (int) $endLimit);
+	echo json_encode($resp);
+	die();
 }
 
 initGPBPConverter();
