bf23de53c2d63e6104e5ea6d9899ffe849158b0a
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2014-09-20 19:41:12 +0530

    Acknowledging contributors Updating copyright

--- Themes/default/LikePosts.template.php
+++ Themes/default/LikePosts.template.php
@@ -4,7 +4,7 @@
 * @package manifest file for Like Posts
 * @version 1.6.1
 * @author Joker (http://www.simplemachines.org/community/index.php?action=profile;u=226111)
-* @copyright Copyright (c) 2012, Siddhartha Gupta
+* @copyright Copyright (c) 2014, Siddhartha Gupta
 * @license http://www.mozilla.org/MPL/MPL-1.1.html
 */
 
@@ -26,7 +26,8 @@
 * Portions created by the Initial Developer are Copyright (C) 2012
 * the Initial Developer. All Rights Reserved.
 *
-* Contributor(s):
+* Contributor(s): Big thanks to all contributor(s)
+* emanuele45 (https://github.com/emanuele45)
 *
 */
 
