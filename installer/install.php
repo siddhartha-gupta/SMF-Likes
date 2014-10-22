1707f26bbf557761b98b90039b856da61e4da9cb
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2014-10-22 11:14:31 +0530

    Version upgrade and installer version update

--- installer/install.php
+++ installer/install.php
@@ -2,7 +2,7 @@
 
 /**
 * @package manifest file for Like Posts
-* @version 2.0
+* @version 2.0.1
 * @author Joker (http://www.simplemachines.org/community/index.php?action=profile;u=226111)
 * @copyright Copyright (c) 2014, Siddhartha Gupta
 * @license http://www.mozilla.org/MPL/MPL-1.1.html
@@ -138,7 +138,7 @@ checkVersion1_5Upgrade();
 checkVersion2_0Upgrade();
 
 // at last just update the mod version
-updateModVersion('2.0');
+updateModVersion('2.0.1');
 
 function checkVersion1_2Upgrade() {
 	global $smcFunc;
